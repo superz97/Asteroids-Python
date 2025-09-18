2dc26def46d53f17a6ce4db337ff2e0139f407ac
superz97 <b.zangar15@gmail.com>
2025-09-18 13:57:29 +0500

    Implement f/b movement

--- main.py
+++ main.py
@@ -11,16 +11,23 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        updateable.update(dt)
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()   
         dt = clock.tick(60) / 1000
 
