625c9da394dc64ce8bd84b87b4bf1f80d218937a
superz97 <b.zangar15@gmail.com>
2025-09-18 14:16:50 +0500

    Implement Asteroid, AsteroidField functionality

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame 
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField 
 
 def main():
     print("Starting Asteroids!")
@@ -9,15 +11,20 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
 
     clock = pygame.time.Clock()
-    dt = 0
 
     updateable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     Player.containers = (updateable, drawable)
+    Asteroid.containers = (asteroids, updateable, drawable)
+    AsteroidField.containers = (updateable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroidField = AsteroidField()
 
     while True:
         for event in pygame.event.get():
